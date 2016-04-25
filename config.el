6622696e142d7d6b08e546e536295e88c5ac1566
flagos <flagospub@gmail.com>
2016-04-25 17:37:01 +0200

    remove useless autopep8 and flycheck

--- config.el
+++ config.el
@@ -41,11 +41,3 @@
 (global-linum-mode t) ;; enable line numbers globally
 (elpy-enable)
 
-
-(require 'py-autopep8)
-(add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
-
-
-(when (require 'flycheck nil t)
-  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
-  (add-hook 'elpy-mode-hook 'flycheck-mode))
