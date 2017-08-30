206512e44cc04de1d79c6d853280ca081fa96faa
flagos <flagospub@gmail.com>
2017-08-30 16:05:15 +0200

    pretty printer on xml

--- config.el
+++ config.el
@@ -37,6 +37,7 @@
     magit
     cl-lib
     js2-mode
+    web-mode
 ))
 
 (mapc #'(lambda (package)
@@ -89,3 +90,19 @@
 
 ;; javascript
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+
+;; pretty print xml
+(defun bf-pretty-print-xml-region (begin end)
+  "Pretty format XML markup in region. You need to have nxml-mode
+http://www.emacswiki.org/cgi-bin/wiki/NxmlMode installed to do
+this.  The function inserts linebreaks to separate tags that have
+nothing but whitespace between them.  It then indents the markup
+by using nxml's indentation rules."
+  (interactive "r")
+  (save-excursion
+      (nxml-mode)
+      (goto-char begin)
+      (while (search-forward-regexp "\>[ \\t]*\<" nil t)
+        (backward-char) (insert "\n"))
+      (indent-region begin end))
+    (message "Ah, much better!"))
