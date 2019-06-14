6ff87b1bba1475060b1fa40c1704c9fa73e3deba
flagos <flagospub@gmail.com>
2019-06-14 14:42:57 +0200

    tab

--- config.el
+++ config.el
@@ -125,3 +125,8 @@ by using nxml's indentation rules."
 (global-set-key (kbd "M-n") 'mc/mark-next-like-this)
 (global-set-key (kbd "M-p") 'mc/mark-previous-like-this)
 (global-set-key (kbd "M-N") 'mc/mark-all-like-this)
+
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 4)
+(setq indent-line-function 'insert-tab)
+
