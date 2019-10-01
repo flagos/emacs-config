229931ad2decdb8ea60c9543322c317ede179eab
flagos <flagospub@gmail.com>
2019-10-01 12:54:10 +0200

    deactivate signatures

--- config.el
+++ config.el
@@ -2,6 +2,7 @@
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 
+(setq package-check-signature nil)
 
 (package-initialize)
 (when (not package-archive-contents)
@@ -141,4 +142,10 @@ by using nxml's indentation rules."
 
 
 ;; selection-highlight
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Ubuntu Mono" :foundry "DAMA" :slant normal :weight normal :height 113 :width normal))))
  '(region ((t (:background "black" :inverse-video t)))))
