074bb2814a972fde40fbb202733290746ce3def0
flagos <flagospub@gmail.com>
2024-04-03 16:03:21 +0200

    Ctrl-K no kill

--- config.el
+++ config.el
@@ -471,6 +471,16 @@ by using nxml's indentation rules."
 ;; Handy key definition
 (define-key global-map "\M-Q" 'unfill-paragraph)
 
+;; Ctrl-K with no kill
+(defun delete-line-no-kill ()
+  (interactive)
+  (delete-region
+   (point)
+   (save-excursion (move-end-of-line 1) (point)))
+  (delete-char 1)
+)
+(global-set-key (kbd "C-k") 'delete-line-no-kill)
+
 ;; pinentry
 (setq epa-pinentry-mode 'loopback)
 (setq epg-pinentry-mode 'loopback)
